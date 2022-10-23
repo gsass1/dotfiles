75a623fa4b406462f92a0a7b5ee5dd8f0bde21df
Gian Saß <gian.sass@mni.thm.de>
2022-10-23 10:43:23 +0200

    Fix .gitignore

---
+++ .config/nvim/after/plugin/colorizer.lua
@@ -0,0 +1,9 @@
+vim.opt.termguicolors = true
+
+require 'colorizer'.setup({
+  'css';
+  'scss';
+  'sass';
+  'javascript';
+  'svelte';
+}, { mode = 'background' })
---
+++ .config/nvim/after/plugin/maps.lua
@@ -0,0 +1,16 @@
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+vim.keymap.set('n', 'fg', builtin.live_grep, {})
+vim.keymap.set('n', 'fb', builtin.buffers, {})
+vim.keymap.set('n', 'fh', builtin.help_tags, {})
+
+vim.keymap.set('n', '<F4>', ':nohlsearch<CR>', {})
+vim.keymap.set('n', '<C-t>', ':tabnew<CR>', {})
+
+vim.keymap.set('n', 'ff', ':NvimTreeToggle<CR>', {})
+
+vim.keymap.set('n', '<Space>', '@q', {})
+vim.keymap.set('n', '<M-l>', '@a', {})
+
+vim.keymap.set('v', '>', '>gv', {})
+vim.keymap.set('v', '<', '<gv', {})
